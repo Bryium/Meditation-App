740545acb6b9b700fc6a34b8056c35acc69b0c99
Bryium <bryiumonyancha@gmail.com>
2025-03-01 02:39:51 +0300

    remove babel.config file

--- simple-meditation/app/index.tsx
+++ simple-meditation/app/index.tsx
@@ -1,17 +1,12 @@
-import { StyleSheet, View, Text } from "react-native";
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function HomeScreen() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View>
+      <Text>App</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
