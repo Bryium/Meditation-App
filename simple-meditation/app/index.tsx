12fe60fc610ce79caff4d2f746351fd9dd26d26d
Bryium <bryiumonyancha@gmail.com>
2025-03-01 02:54:25 +0300

    add background image

--- simple-meditation/app/index.tsx
+++ simple-meditation/app/index.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import React from "react";
+
+import beachImage from "@/assets/meditation-images/beach.webp";
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Text style={{ fontSize: 24, color: "white", fontWeight: "bold" }}>
+          App
+        </Text>
+      </ImageBackground>
     </View>
   );
 };
