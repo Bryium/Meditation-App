716eae8e6a04c07a4b0db689dd8e658c50b84bb7
Bryium <bryiumonyancha@gmail.com>
2025-03-01 15:32:08 +0300

    change the text

--- simple-meditation/app/index.tsx
+++ simple-meditation/app/index.tsx
@@ -1,4 +1,5 @@
-import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import beachImage from "@/assets/meditation-images/beach.webp";
@@ -12,16 +13,16 @@ const App = () => {
         className="flex-1"
       >
         <LinearGradient
-          className="flex-1 justify-center"
+          className="flex-1 "
           colors={["rgba(0,0,0,0.6)", "rgba(0,0,0,0.8)"]}
         >
-          <SafeAreaView>
+          <SafeAreaView className="flex-1 px-1 justify-between">
             <View>
               <Text className="text-center text-white font-bold text-4xl">
                 Simple Meditation
               </Text>
               <Text className="text-center text-white text-2xl mt-3">
-                Simple Meditation for Everyone
+                Simplifying Meditation for Everyone
               </Text>
             </View>
           </SafeAreaView>
