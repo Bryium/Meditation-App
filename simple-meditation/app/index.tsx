9de28157c2d5294b63a613a45f82dcc750a1860b
Bryium <bryiumonyancha@gmail.com>
2025-03-01 15:50:39 +0300

    add margins to the button

--- simple-meditation/app/index.tsx
+++ simple-meditation/app/index.tsx
@@ -18,7 +18,7 @@ const App = () => {
           className="flex-1 "
           colors={["rgba(0,0,0,0.6)", "rgba(0,0,0,0.8)"]}
         >
-          <SafeAreaView className="flex-1 px-1 justify-between">
+          <SafeAreaView className="flex-1 px-1 mx-5 my-12 justify-between">
             <View>
               <Text className="text-center text-white font-bold text-4xl">
                 Simple Meditation
