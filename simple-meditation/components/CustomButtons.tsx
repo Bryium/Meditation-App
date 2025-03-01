c763845225fdb57a0aee7d0006622b7cf61787f0
Bryium <bryiumonyancha@gmail.com>
2025-03-01 15:44:02 +0300

    style the touchableopacity

--- simple-meditation/components/CustomButtons.tsx
+++ simple-meditation/components/CustomButtons.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 interface CustomButtonsProps {
@@ -8,11 +8,22 @@ interface CustomButtonsProps {
   containerStyles?: object;
 }
 
-const CustomButtons = () => {
+const CustomButtons = ({
+  onPress,
+  title,
+  textStyles = "",
+  containerStyles = "",
+}) => {
   return (
-    <View>
-      <Text>CustomButtons</Text>
-    </View>
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className={
+        "bg-white rounded-xl min-h-[62px] justify-center items-center ${containerStyles}"
+      }
+      onPress={onPress}
+    >
+      <Text className={"font-semibold text-lg"}>CustomButtons</Text>
+    </TouchableOpacity>
   );
 };
 
