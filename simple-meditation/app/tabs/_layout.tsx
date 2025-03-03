48821a86e3910aa970f1ba7ab58ba0be172c15eb
Bryium <bryiumonyancha@gmail.com>
2025-03-03 15:32:41 +0300

    update tabs and index screens

--- simple-meditation/app/tabs/_layout.tsx
+++ simple-meditation/app/tabs/_layout.tsx
@@ -1,12 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
 
-const Tabslayout = () => {
+const TabsLayout = () => {
   return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveBackgroundColor: Colors.primary,
+      }}
+    >
+      <Tabs.Screen
+        name="nature-meditate"
+        options={{ tabBarLabel: "Meditate" }}
+      />
+    </Tabs>
   );
 };
 
