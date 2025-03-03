11189055c3bcec0aba074f7546c4ba7ddb15639b
Bryium <bryiumonyancha@gmail.com>
2025-03-03 15:25:18 +0300

    create new file layout.tsx

---
+++ simple-meditation/app/tabs/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const Tabslayout = () => {
+  return (
+    <View>
+      <Text>Tabs</Text>
+    </View>
+  );
+};
+
+export default TabsLayout;
