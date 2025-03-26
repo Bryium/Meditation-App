5a57803628520f846435b9e37c23dc1d04e45e79
Bryium <bryiumonyancha@gmail.com>
2025-03-26 17:46:11 +0300

    add resubale component guidedaffirmations

---
+++ simple-meditation/components/GuidedAffirmationsGallery.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const GuidedAffirmationsGalleryProps = () => {
+  return (
+    <View>
+      <Text>GuidedAffirmationsGalleryProps</Text>
+    </View>
+  );
+};
+
+export default GuidedAffirmationsGalleryProps;
