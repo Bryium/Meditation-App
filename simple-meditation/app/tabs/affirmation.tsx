db4074eebf71c7361688ff4466dd4f3dd90f0001
Bryium <bryiumonyancha@gmail.com>
2025-03-26 17:43:53 +0300

    add text to scrollview

--- simple-meditation/app/tabs/affirmation.tsx
+++ simple-meditation/app/tabs/affirmation.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import AppGradient from "@/components/AppGradient";
 
@@ -6,7 +6,12 @@ const Affirmations = () => {
   return (
     <View className="flex-1">
       <AppGradient colors={["#2e1f58", "#54426b", "#a790af"]}>
-        <Text>Affirmations</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text className="tetx-zinc-50 text-3xl font-bold">
+            Change your beliefs with affirmations
+          </Text>
+          <View></View>
+        </ScrollView>
       </AppGradient>
     </View>
   );
