03f15df9818c11cd6832f563c54b4278a9f11e79
Bryium <bryiumonyancha@gmail.com>
2025-03-26 17:49:50 +0300

    create new directory and new file

--- simple-meditation/components/GuidedAffirmationsGallery.tsx
+++ simple-meditation/components/GuidedAffirmationsGallery.tsx
@@ -1,6 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+interface GuidedAffirmationsGalleryProps {
+  title: string;
+  previews:
+}
 const GuidedAffirmationsGalleryProps = () => {
   return (
     <View>
