47b3dccd7eb328d789b3e8a345167d5ea751d318
Bryium <bryiumonyancha@gmail.com>
2025-04-08 19:53:13 +0300

    add uselocalsearchparams for routing

--- simple-meditation/app/tabs/affirmations/_layout.tsx
+++ simple-meditation/app/tabs/affirmations/_layout.tsx
@@ -6,6 +6,7 @@ const AffirmationLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="[itemId]" options={{ headerShown: false }} />
     </Stack>
   );
 };
