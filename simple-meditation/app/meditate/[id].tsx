78cc375555899e607061111028b3e467032437c1
Bryium <bryiumonyancha@gmail.com>
2025-04-21 19:30:59 +0300

    add countdown timer logic with cleanup in meditation screen

--- simple-meditation/app/meditate/[id].tsx
+++ simple-meditation/app/meditate/[id].tsx
@@ -1,16 +1,38 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-import React from "react";
 
 import MEDITATION_IMAGES from "@/constants/meditation-images";
 import AppGradient from "@/components/AppGradient";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { router, useLocalSearchParams } from "expo-router";
 import CustomButtons from "@/components/CustomButtons";
+import { useEffect, useState } from "react";
 
 const MeditateScreen = () => {
   const { id } = useLocalSearchParams();
 
   const imageIndex = (Number(id) - 1) % MEDITATION_IMAGES.length;
+
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setMeditating] = useState(false);
+
+  useEffect(() => {
+    let timerID: NodeJS.Timeout;
+
+    //Exit
+
+    if (secondsRemaining === 0) {
+      setMeditating(false);
+      return;
+    }
+
+    timerID = setTimeout(() => {
+      setSecondsRemaining(secondsRemaining - 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [secondsRemaining]);
 
   return (
     <View className="flex-1">
@@ -28,13 +50,15 @@ const MeditateScreen = () => {
           </Pressable>
           <View className="flex-1 justify-center">
             <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center ">
-              <Text className="text-4xl text-blue-800">00:00</Text>
+              <Text className="text-4xl text-blue-800">
+                00:{secondsRemaining}
+              </Text>
             </View>
           </View>
           <View className="mb-5">
             <CustomButtons
               title="start Meditation"
-              onPress={() => console.log("meditate")}
+              onPress={() => setMeditating(true)}
             />
           </View>
         </AppGradient>
