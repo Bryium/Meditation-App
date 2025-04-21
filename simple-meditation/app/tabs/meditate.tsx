80ad3a94ae98c7e2c636e25786eeeeb228058fa0
Bryium <bryiumonyancha@gmail.com>
2025-04-21 16:54:53 +0300

    add background image to meditate

--- simple-meditation/app/tabs/meditate.tsx
+++ simple-meditation/app/tabs/meditate.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import React from "react";
 
-const meditate = () => {
+import MEDITATION_IMAGES from "@/constants/meditation-images";
+
+const MeditateScreen = () => {
   return (
-    <View>
-      <Text>meditate</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={MEDITATION_IMAGES[0]}
+        resizeMode="cover"
+        className="flex-1 justify-center items-center"
+      >
+        <Text className="text-white text-2xl font-bold">Test</Text>
+      </ImageBackground>
+      <Text className="text-center mt-4">meditate</Text>
     </View>
   );
 };
 
-export default meditate;
+export default MeditateScreen;
