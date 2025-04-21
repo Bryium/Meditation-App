f3c1bf805cfe63d602277919290aebff1a53a8bc
Bryium <bryiumonyancha@gmail.com>
2025-04-21 19:15:22 +0300

    Add meditation timer display and start button to UI

--- simple-meditation/app/meditate/[id].tsx
+++ simple-meditation/app/meditate/[id].tsx
@@ -5,11 +5,10 @@ import MEDITATION_IMAGES from "@/constants/meditation-images";
 import AppGradient from "@/components/AppGradient";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { router, useLocalSearchParams } from "expo-router";
+import CustomButtons from "@/components/CustomButtons";
 
 const MeditateScreen = () => {
   const { id } = useLocalSearchParams();
-
-  console.log("received id:", id);
 
   const imageIndex = (Number(id) - 1) % MEDITATION_IMAGES.length;
 
@@ -27,6 +26,17 @@ const MeditateScreen = () => {
           >
             <AntDesign name="leftcircleo" size={50} color="white" />
           </Pressable>
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center ">
+              <Text className="text-4xl text-blue-800">00:00</Text>
+            </View>
+          </View>
+          <View className="mb-5">
+            <CustomButtons
+              title="start Meditation"
+              onPress={() => console.log("meditate")}
+            />
+          </View>
         </AppGradient>
       </ImageBackground>
     </View>
