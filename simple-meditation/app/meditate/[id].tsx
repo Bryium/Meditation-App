6cef2a92b40e3ae867cc9689a5d0fbb0eb6fa1a9
Bryium <bryiumonyancha@gmail.com>
2025-05-10 15:24:11 +0300

    add audio playback to meditation screen

--- simple-meditation/app/meditate/[id].tsx
+++ simple-meditation/app/meditate/[id].tsx
@@ -1,19 +1,22 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-
 import MEDITATION_IMAGES from "@/constants/meditation-images";
 import AppGradient from "@/components/AppGradient";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { router, useLocalSearchParams } from "expo-router";
 import CustomButtons from "@/components/CustomButtons";
 import { useEffect, useState } from "react";
+import { Audio } from "expo-av";
+import { AUDIO_FILES, MEDITATION_DATA } from "@/constants/MeditationData";
 
 const MeditateScreen = () => {
   const { id } = useLocalSearchParams();
-
-  const imageIndex = (Number(id) - 1) % MEDITATION_IMAGES.length;
+  const index = Number(id) - 1;
+  const imageIndex = index % MEDITATION_IMAGES.length;
 
   const [secondsRemaining, setSecondsRemaining] = useState(10);
   const [isMeditating, setMeditating] = useState(false);
+  const [audioSound, setSound] = useState<Audio.Sound>();
+  const [isPlayingAudio, setplayingAudio] = useState(false);
 
   useEffect(() => {
     let timerID: NodeJS.Timeout;
@@ -34,7 +37,38 @@ const MeditateScreen = () => {
     };
   }, [secondsRemaining, isMeditating]);
 
-  //Format time left to ensure two digits are displayed
+  useEffect(() => {
+    return () => {
+      audioSound?.unloadAsync();
+    };
+  }, [audioSound]);
+
+  const toggleMeditationSessionStatus = async () => {
+    if (secondsRemaining === 0) setSecondsRemaining(10);
+    setMeditating((prev) => !prev);
+    await toggleSound();
+  };
+
+  const toggleSound = async () => {
+    const sound = audioSound ?? (await initializeSound());
+
+    const status = await sound.getStatusAsync();
+
+    if (status?.isLoaded && !isPlayingAudio) {
+      await sound.playAsync();
+      setplayingAudio(true);
+    } else if (status?.isLoaded) {
+      await sound.pauseAsync();
+      setplayingAudio(false);
+    }
+  };
+
+  const initializeSound = async () => {
+    const audioFileName = MEDITATION_DATA[index].audio;
+    const { sound } = await Audio.Sound.createAsync(AUDIO_FILES[audioFileName]);
+    setSound(sound);
+    return sound;
+  };
 
   const formattedTimeMinutes = String(
     Math.floor(secondsRemaining / 60)
@@ -56,7 +90,7 @@ const MeditateScreen = () => {
             <AntDesign name="leftcircleo" size={50} color="white" />
           </Pressable>
           <View className="flex-1 justify-center">
-            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center ">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
               <Text className="text-4xl text-blue-800 font-rmono">
                 {formattedTimeMinutes}:{formattedTimeSeconds}
               </Text>
@@ -64,8 +98,8 @@ const MeditateScreen = () => {
           </View>
           <View className="mb-5">
             <CustomButtons
-              title="start Meditation"
-              onPress={() => setMeditating(true)}
+              title="Start Meditation"
+              onPress={toggleMeditationSessionStatus}
             />
           </View>
         </AppGradient>
